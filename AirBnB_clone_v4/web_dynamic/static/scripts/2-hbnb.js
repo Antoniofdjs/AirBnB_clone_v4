622d8c7910d562d56aece69fbfd922c3249a0ee8
Ltoro9 <louistororosario@gmail.com>
2024-05-29 10:10:03 -0400

    add a condition if status if not "ok" remove class

--- AirBnB_clone_v4/web_dynamic/static/scripts/2-hbnb.js
+++ AirBnB_clone_v4/web_dynamic/static/scripts/2-hbnb.js
@@ -27,10 +27,11 @@ $(document).ready(function () {
     });
   });
 
-  $.get("http://127.0.0.1:5001/api/v1/status/", function (data) {
-    console.log(data.status);
+  $.get("http://127.0.0.1:5001/api/v1/status/", data => {
     if (data.status === "OK") {
       $("#api_status").addClass("available");
+    }  else {
+      $('#api_status').removeClass("available");
     }
   });
 });
